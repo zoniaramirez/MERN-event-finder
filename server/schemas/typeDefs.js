2e68daf7a10dfcf71c079e410fab58711ede0b68
Zonia Ramirez <jslnramirez@gmail.com>
2024-10-02 20:26:00 -0400

    updated typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,7 @@ const typeDefs = `#graphql
     username: String
     email: String
     password: String
+    saveEvent: [Event]!
   }
 
   type Auth {
@@ -17,10 +18,20 @@ const typeDefs = `#graphql
     searchUsers(term: String!): [User]!
     me: User
   }
+  
+  type Event {
+    _id: ID
+    description: String
+    title: String
+    image: String
+    link: String
+  }
 
   type Mutation {
     addUser(email:String!, username:String!, password:String!): Auth
     login(email:String!, password:String!): Auth
+    saveEvent(eventId: ID!): User
+    removeEvent(eventId: ID!): User
   }
 `;
 
